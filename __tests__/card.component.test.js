356230182305e494217b9cc770e1d8b0879d9920
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2021-12-16 03:01:26 +0800

    added tests for 2 cases

---
+++ __tests__/card.component.test.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+
+import 'react-native';
+import React from 'react';
+import Card from '../src/components/card/card.component';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+it('renders correctly if points property is 99999', () => {
+  const tree = renderer.create(<Card points={99999} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly if no points property', () => {
+  const tree = renderer.create(<Card />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
