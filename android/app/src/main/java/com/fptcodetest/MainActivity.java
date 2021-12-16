ef11ed9067cf08d4e3a2c3a87193a038c7b71ddc
Red De Guzman <reddeguzman@Reds-MacBook-Pro.local>
2021-12-16 08:44:43 +0800

    rename

--- android/app/src/main/java/com/fptcodetest/MainActivity.java
+++ android/app/src/main/java/com/fptcodetest/MainActivity.java
@@ -1,4 +1,4 @@
-package com.nftcodetest;
+package com.fptcodetest;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "nftcodetest";
+    return "fptcodetest";
   }
 }
--- ios/fptcodetestTests/fptcodetestTests.m
+++ ios/fptcodetestTests/fptcodetestTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface nftcodetestTests : XCTestCase
+@interface fptcodetestTests : XCTestCase
 
 @end
 
-@implementation nftcodetestTests
+@implementation fptcodetestTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
